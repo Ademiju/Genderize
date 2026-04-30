d7d15fd2b0c3a517c25ee664136743f9c975bcdf
AdemijuT <ademijuwonlotaiwo@gmail.com>
2026-04-30 23:49:03 +0100

    refactor(api): extend token expiry

--- src/main/java/com/app/Genderize/config/auth/JwtService.java
+++ src/main/java/com/app/Genderize/config/auth/JwtService.java
@@ -33,7 +33,7 @@ public class JwtService {
                 .setSubject(user.getId().toString())
                 .claim("role", user.getRole())
                 .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(Instant.now().plusSeconds(1800)))
+                .setExpiration(Date.from(Instant.now().plusSeconds(3000)))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
